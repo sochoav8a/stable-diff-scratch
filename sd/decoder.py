e1e158d4f39563b21bec26815a0d4e34835584e1
sochoav8a <sochoav30@gmail.com>
2025-08-18 01:51:24 -0500

    initialize decoder

---
+++ sd/decoder.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from attention import SelfAttention
+
+class VAE_ResidualBlock(nn.Module):
+    ## TODO: Implement the VAE_ResidualBlock class 
+    
+    
