8854cb9370c6ef310f514a70a83bc7373839e42a
sochoav8a <sochoav30@gmail.com>
2025-08-18 15:12:14 -0500

    add self attention decoder

--- sd/decoder.py
+++ sd/decoder.py
@@ -3,7 +3,74 @@ from torch import nn
 from torch.nn import functional as F
 from attention import SelfAttention
 
+
+class VAE_AttentionBlock(nn.Module):
+    def __init__(self, channels: int):
+        super().__init__()
+        self.groupnorm = nn.GroupNorm(32, channels)
+        self.attention = SelfAttention(1, channels)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x: (Batch_Size, Features, Height, Width)
+        
+        residue = x 
+        
+        n, c, h, w = x.shape
+        
+        # (Batch_Size, Features, Height * Width) - (batch_size, features, height * width)
+        x = x.view(n, c, h * w) 
+        
+        # (Batch_Size, Features, Height * Width) - (batch_size, height * width, features)
+        x = x.transpose(-1, -2)  
+        
+        # !!doesnt change the shape of the input tensor!!
+        x = self.attention(x)
+        
+        # (Batch_Size, Height * Width, Features) - (batch_size, features, height * width)
+        x = x.transpose(-1, -2)  
+        
+        # (Batch_Size, Features, Height * Width) - (batch_size, features, height, width)
+        x = x.view(n, c, h, w)
+        
+        x =+ residue
+        
+        return x
+        
+        
+        
 class VAE_ResidualBlock(nn.Module):
-    ## TODO: Implement the VAE_ResidualBlock class 
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.groupnorm_1 = nn.GroupNorm(32, in_channels)
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        
+        self.groupnorm_2 = nn.GroupNorm(32, out_channels)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        
+        #if the residual connection is not the same size, we need to adjust it
+        if in_channels == out_channels:
+            self.residual_layer = nn.Identity()
+        else:
+            self.residual_layer = nn.Conv2d(in_channels, out_channels, kernel_size=1, padding=0)
+            
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        #( x: [batch_size, in_channels, height, width] )
+        #Note that the forward doesnt change the shape of the input tensor!!!!
+        residue = x
+        
+        x = self.groupnorm_1(x)
+        
+        x = F.silu(x)
+        
+        x = self.conv_1(x)
+        
+        x = self.groupnorm_2(x)
+        
+        x = F.silu(x)
+        
+        x = self.conv_2(x)
+        
+        x = x + self.residual_layer(residue)
+        
     
     
